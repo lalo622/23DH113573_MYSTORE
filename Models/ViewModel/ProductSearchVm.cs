2d19c287dc49c252e66dcfdf4e2ad1e62ca3879a
lalo622 <tkonn552@gmail.com>
2024-10-20 22:58:28 +0700

    2

--- Models/ViewModel/ProductSearchVm.cs
+++ Models/ViewModel/ProductSearchVm.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PagedList.Mvc;
 
 namespace _23DH113573_MyStore.Models.ViewModel
 {
-    public class ProductSearchVm
+    public class ProductSearchVM
     {
+        public string SearchTerm { get; set; }
+        //public List<Product> Products { get; set; }
 
+        public string SortOrder { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; } = 10;
+
+        public PagedList.IPagedList<Product> Products { get; set; }
     }
 }
