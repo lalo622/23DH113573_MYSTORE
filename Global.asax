6cc5b0aa93c8039253aa11528ed8e730597d08dc
HOME <HOME@LAPTOP-RN6U1SG4>
2024-10-20 21:03:28 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH113573_MyStore.MvcApplication" Language="C#" %>
---
+++ Models/ViewModel/ProductSearchVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _23DH113573_MyStore.Models.ViewModel
+{
+    public class ProductSearchVm
+    {
+
+    }
+}
